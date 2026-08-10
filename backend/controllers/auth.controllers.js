3055c41c3b812d20f9b5fa8c793dd66d79160a75
sachet26 <sachetjain32@gmail.com>
2026-08-10 22:05:40 +0530

    Update auth.controllers.js

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -54,7 +54,7 @@ export const login = async(req, res)=>{
     res.cookie("token", token, {
         httpOnly : true,
         maxAge: 7*24*60*60*1000,
-        sameSite:"strict",
+        sameSite:"none",
         secure:process.env.NODE_ENVIRONMENT === "production"
     })
     return res.status(201).json(user)
